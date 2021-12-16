b006285c64eb9cf195453088ea653339dd1f3c9d
domnick1910 <57629162+domnick1910@users.noreply.github.com>
2021-12-16 15:02:34 +0100

    pridanie metody k vyhlaseniu vyhry

--- src/sk/uniza/fri/Logika.java
+++ src/sk/uniza/fri/Logika.java
@@ -1,5 +1,7 @@
 package sk.uniza.fri;
 
+import java.util.ArrayList;
+
 /**
  * 2. 12. 2021 - 19:08
  *
@@ -7,18 +9,20 @@ package sk.uniza.fri;
  */
 public class Logika {
     private HraciaPlocha hraciaPlocha;
-    private Hrac[] hrac;
     private int pocetHracov;
     private int velkostHracejPlochy;
+    private boolean jeKoniec;
 
-    public Logika(int pocetHracov) {
+    public Logika(int pocetHracov, ArrayList<Hrac> hraci) {
+        this.jeKoniec = false;
         this.vypocetHracejPlochy(pocetHracov);
         this.hraciaPlocha = new HraciaPlocha(this.velkostHracejPlochy);
     }
 
     public void vypocetHracejPlochy(int pocetHracov) {
+        // podmienka pre zaporne cisla dat do main class
         if (pocetHracov == 1) {
-            this.velkostHracejPlochy= 3;
+            this.velkostHracejPlochy = 3;
         }
 
         if (pocetHracov > 3) {
@@ -30,12 +34,38 @@ public class Logika {
     // da na vyber ci obnovit hraciu plochu, alebo ma skoncit
     public void obnovaHracejPlochy(String znak) {
         switch (znak) {
-            case "Y": System.out.println();
+            case "Y": this.hraciaPlocha.vykresleniePola();
                 break;
-            case "N": System.out.println("A");
+            case "N": this.jeKoniec = true;
                 break;
             default: System.out.println("Zlý znak");
                 break;
         }
     }
+
+    public boolean vyhra(String znak) {
+        if (!this.hraciaPlocha.hladanieVRiadku(znak)) {
+            return true;
+        }
+
+        if (!this.hraciaPlocha.hladanieVStlpci(znak)) {
+            return true;
+        }
+
+        if (!this.hraciaPlocha.hladanieDiagonaly(znak)) {
+            return true;
+        }
+
+        if (!this.hraciaPlocha.hladanieAntiDiagonaly(znak)) {
+            return  true;
+        }
+
+        return  false;
+    }
+
+    public boolean getJeKoniec() {
+        return this.jeKoniec;
+    }
+
+
 }
